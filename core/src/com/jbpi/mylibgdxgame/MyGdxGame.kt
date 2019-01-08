4fa75e2986fa7c583203e2027b91e2519e3698b3
Jake Piatkowski <jake.piatkowski@gmail.com>
2019-01-08 22:01:59 +0000

    WiP Show bucket and play music

--- core/src/com/jbpi/mylibgdxgame/MyGdxGame.kt
+++ core/src/com/jbpi/mylibgdxgame/MyGdxGame.kt
@@ -2,29 +2,62 @@ package com.jbpi.mylibgdxgame
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
 
 class MyGdxGame : ApplicationAdapter() {
-    lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+
+    lateinit var textureDrop: Texture
+    lateinit var textureBucket: Texture
+    lateinit var soundDrop: Sound
+    lateinit var musicBackground: Music
+
+    lateinit var rectangleBucket: Rectangle
+
+    lateinit var camera: OrthographicCamera
+    lateinit var spriteBatch: SpriteBatch
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+
+        textureDrop = Texture(Gdx.files.internal("droplet.png"))
+        textureBucket = Texture(Gdx.files.internal("bucket.png"))
+        soundDrop = Gdx.audio.newSound(Gdx.files.internal("drop.wav"))
+        musicBackground = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"))
+
+        musicBackground.isLooping = true
+//        musicBackground.play()
+
+        rectangleBucket = Rectangle()
+        rectangleBucket.width = 64f
+        rectangleBucket.height = 64f
+        rectangleBucket.x = 800f / 2f - 64f / 2f
+        rectangleBucket.y = 20f
+
+        camera = OrthographicCamera()
+        camera.setToOrtho(false, 800f, 480f)
+
+        spriteBatch = SpriteBatch()
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
 
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
+        spriteBatch.projectionMatrix = camera.combined
+        spriteBatch.begin()
+        spriteBatch.draw(textureBucket, rectangleBucket.x, rectangleBucket.y)
+        spriteBatch.end()
+
+        camera.update()
     }
+//
+//    override fun dispose() {
+//        batch.dispose()
+//        img.dispose()
+//    }
 }
