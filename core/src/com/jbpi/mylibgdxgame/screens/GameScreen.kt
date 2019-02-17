9c9a7c7a3b345084df187ea73be4c44ec0009454
Jake Piatkowski <jake.piatkowski@gmail.com>
2019-02-17 21:23:53 +0000

    Move actual gameplay to specific Screen

--- core/src/com/jbpi/mylibgdxgame/screens/GameScreen.kt
+++ core/src/com/jbpi/mylibgdxgame/screens/GameScreen.kt
@@ -1,18 +1,18 @@
-package com.jbpi.mylibgdxgame
+package com.jbpi.mylibgdxgame.screens
 
-import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.Screen
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.TimeUtils
+import com.jbpi.mylibgdxgame.DropGame
 
 const val SCREEN_WIDTH = 800f
 const val SCREEN_HEIGHT = 480f
@@ -24,30 +24,23 @@ const val RECTANGLE_DROPLET_HEIGHT = 64f
 
 const val INTERVAL_BETWEEN_DROPLETS = 1000000000
 
-class MyGdxGame : ApplicationAdapter() {
+class GameScreen (private val game: DropGame) : Screen {
 
-    private lateinit var textureDroplet: Texture
-    private lateinit var textureBucket: Texture
-    private lateinit var soundDroplet: Sound
-    private lateinit var musicBackground: Music
+    private var textureDroplet: Texture = Texture(Gdx.files.internal("droplet.png"))
+    private var textureBucket: Texture = Texture(Gdx.files.internal("bucket.png"))
+    private var soundDroplet: Sound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"))
+    private var musicBackground: Music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"))
 
     private val rectangleBucket = Rectangle()
     private val rectanglesDroplets = com.badlogic.gdx.utils.Array<Rectangle>()
 
     private val camera = OrthographicCamera()
 
-    private lateinit var spriteBatch: SpriteBatch
-
     private var lastDropletTime: Long = 0
 
     private val convertedTouchPosition: Vector3 = Vector3()
 
-    override fun create() {
-
-        textureDroplet = Texture(Gdx.files.internal("droplet.png"))
-        textureBucket = Texture(Gdx.files.internal("bucket.png"))
-        soundDroplet = Gdx.audio.newSound(Gdx.files.internal("drop.wav"))
-        musicBackground = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"))
+    init {
 
         musicBackground.isLooping = true
 //        musicBackground.play()
@@ -58,20 +51,22 @@ class MyGdxGame : ApplicationAdapter() {
         rectangleBucket.y = 20f
 
         camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT)
-
-        spriteBatch = SpriteBatch()
     }
 
-    override fun render() {
+    override fun hide() {}
+
+    override fun show() {}
+
+    override fun render(delta: Float) {
 
         Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        spriteBatch.projectionMatrix = camera.combined
-        spriteBatch.begin()
-        spriteBatch.draw(textureBucket, rectangleBucket.x, rectangleBucket.y)
-        rectanglesDroplets.forEach { spriteBatch.draw(textureDroplet, it.x, it.y) }
-        spriteBatch.end()
+        game.spriteBatch.projectionMatrix = camera.combined
+        game.spriteBatch.begin()
+        game.spriteBatch.draw(textureBucket, rectangleBucket.x, rectangleBucket.y)
+        rectanglesDroplets.forEach { game.spriteBatch.draw(textureDroplet, it.x, it.y) }
+        game.spriteBatch.end()
 
         camera.update()
 
@@ -140,15 +135,6 @@ class MyGdxGame : ApplicationAdapter() {
         }
     }
 
-    override fun dispose() {
-
-        textureDroplet.dispose()
-        textureBucket.dispose()
-        soundDroplet.dispose()
-        musicBackground.dispose()
-        spriteBatch.dispose()
-    }
-
     private fun spawnDroplet() {
 
         if (TimeUtils.nanoTime() - lastDropletTime <= INTERVAL_BETWEEN_DROPLETS) {
@@ -166,4 +152,18 @@ class MyGdxGame : ApplicationAdapter() {
 
         lastDropletTime = TimeUtils.nanoTime()
     }
+
+    override fun dispose() {
+
+        textureDroplet.dispose()
+        textureBucket.dispose()
+        soundDroplet.dispose()
+        musicBackground.dispose()
+    }
+
+    override fun pause() {}
+
+    override fun resume() {}
+
+    override fun resize(width: Int, height: Int) {}
 }
